e79301bd8325f6ae8e3f07d5c859df2f4b8026cc
Anjana Maharjan <zanjana150@gmail.com>
2025-05-02 17:28:51 +0545

    ts file

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+// src/types.d.ts
+
+declare module 'react-simple-maps' {
+    import React from 'react';
+  
+    export const ComposableMap: React.FC<any>;
+    export const Geographies: React.FC<any>;
+    export const Geography: React.FC<any>;
+    export const Marker: React.FC<any>;
+    export const ZoomableGroup: React.FC<any>;
+  }
